b9bbad07a59fe571e568d2bf9905c0c4006b9b8b
Daniel Feijo <daniellfeijo@gmail.com>
2014-12-17 22:02:21 -0300

    Exercicio01 pronto e comentado

---
+++ exercicio01.java
@@ -0,0 +1,23 @@
+//Classe Funcionário
+//Uma Classe é uma receita de bolo
+//O objeto instanciado é o bolo feito a partir da classe
+class Funcionário{
+	//atributos da classe
+	String nome;
+	String departamento;
+	double salario;
+	String dataEntrada;
+	String rg;
+	
+	//metodo recebeAumento sem retorno "void"
+	//recebe como parametro o percetual de aumento	
+	void recebeAumento (double percentual){
+		this.salario += this.salario*percentual;
+
+	}
+	//metodo calculaGanhoAnual com retorno double
+	double calculaGanhoAnual(){
+		double ganhoAnual = this.salario*12;
+		return ganhoAnual;		
+	}
+}
